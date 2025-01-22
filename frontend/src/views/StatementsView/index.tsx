eb380a69fb45e2e6dea55c9652509f1977b0b006
Red <red.pace.dev@gmail.com>
2025-01-22 19:52:10 +0100

    feat: Added generic views of `ProfileView` and `StatementView` for support `requiredAssets` exported arr

---
+++ frontend/src/views/StatementsView/index.tsx
@@ -0,0 +1,14 @@
+import type { AssetKey } from "@/utils/assetPreloader";
+
+export const requiredAssets: AssetKey[] = [];
+
+const StatementsView = () => {
+  return (
+    <div>
+      <h1>Statements Page</h1>
+      <h3>Please consider this as a heavy view with lots of components, data and animations.</h3>
+    </div>
+  );
+};
+
+export default StatementsView;
