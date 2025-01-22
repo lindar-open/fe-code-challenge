1118150da2ed4a9833645068c84374bc3eeb5915
Red <red.pace.dev@gmail.com>
2025-01-22 22:50:45 +0100

    feat: Added stock types

--- frontend/src/lib/types/index.ts
+++ frontend/src/lib/types/index.ts
@@ -1,2 +1,1 @@
-import * as stockTypes from './stockTypes';
-export { stockTypes };
+export * from './stockTypes';
--- frontend/src/lib/types/stockTypes.ts
+++ frontend/src/lib/types/stockTypes.ts
@@ -1,1 +1,7 @@
 export type Bias = 'POSITIVE' | 'NEUTRAL' | 'NEGATIVE';
+
+export type TrendType = 'UP' | 'DOWN' | null;
+
+export interface PricesState {
+  [key: string]: number;
+}
