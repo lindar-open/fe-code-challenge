7417893ae2e405392fa38da2452d70ee890afef7
Red <red.pace.dev@gmail.com>
2025-01-22 22:46:16 +0100

    chore: Move row component

---
+++ frontend/src/components/Row/index.tsx
@@ -0,0 +1,25 @@
+import { memo } from 'react';
+import type { ReactNode } from 'react';
+import './row.css';
+
+type RowProps = {
+  spacing?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+  children: ReactNode;
+  className?: string;
+};
+
+const spacingMap = {
+  xs: 'row--spacingXs',
+  sm: 'row--spacingSm',
+  md: 'row--spacingMd',
+  lg: 'row--spacingLg',
+  xl: 'row--spacingXl'
+};
+
+export const Row = memo(({ spacing = 'xs', className, children }: RowProps) => {
+  return (
+    <div className={`row ${spacingMap[spacing]}${className ? ` ${className}` : ''}`}>
+      {children}
+    </div>
+  );
+});
