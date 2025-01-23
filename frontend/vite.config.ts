c45dbc097162e332c5433c4fb34d0211b50dcbd6
Red <red.pace.dev@gmail.com>
2025-01-23 01:36:04 +0100

    feat: Updated vite config with css, optimizingDeps, and more optimizations

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,62 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 // @ts-ignore
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
-    react(),
+    react({
+      fastRefresh: true
+    }),
     tsconfigPaths(),
     svgr({
       svgrOptions: {
         exportType: 'named',
         ref: true,
-        svgo: false,
+        svgo: true,
+        svgoConfig: {
+          multipass: true,
+          plugins: [
+            'preset-default',
+            'removeDimensions',
+            {
+              name: 'removeViewBox',
+              active: false
+            },
+            {
+              name: 'removeAttrs',
+              params: {
+                attrs: '(data-name)'
+              }
+            }
+          ]
+        },
         titleProp: true
       },
       include: '**/*.svg'
-    })
+    }),
+    splitVendorChunkPlugin()
   ],
   optimizeDeps: {
-    include: ['react', 'react-dom'],
+    include: [
+      'react',
+      'react-dom',
+      'react-router-dom',
+      'redux',
+      'react-redux',
+      '@reduxjs/toolkit',
+      'recharts'
+    ],
+    exclude: ['@vitejs/plugin-react']
   },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        },
-      },
+
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+      generateScopedName:
+        mode === 'production' ? '[hash:base64:8]' : '[name]__[local]__[hash:base64:5]'
     },
-  },
-});
+  }
+}));
