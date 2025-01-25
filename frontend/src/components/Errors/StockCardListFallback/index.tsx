f12f348b6be9c3fadcc13aeae8de3fc3243f1952
Red <red.pace.dev@gmail.com>
2025-01-25 12:14:14 +0100

    feat: Integrated CSS modules for `StockCardListFallback`

--- frontend/src/components/Errors/StockCardListFallback/index.tsx
+++ frontend/src/components/Errors/StockCardListFallback/index.tsx
@@ -1,11 +1,17 @@
-export const StockCardListFallback = () => (
-  <div className="stockCardList stockCardList--error">
-    <h3>Something went wrong with stock cards</h3>
+import { memo } from 'react';
+import globalErrorBoundaryStyles from '@/components/ErrorBoundary/ErrorBoundary.module.css'
+import styles from './StockCardListFallback.module.css';
+
+export const StockCardListFallback = memo(() => (
+  <div className={styles.cardListError}>
+    <h3 className={styles.cardListTitle}>Something went wrong with stock cards</h3>
     <button 
       onClick={() => window.location.reload()}
-      className="stockCardList__retry-button"
+      className={globalErrorBoundaryStyles.retryButton}
     >
       Retry
     </button>
   </div>
-);
+));
+
+StockCardListFallback.displayName = 'StockCardListFallback';
