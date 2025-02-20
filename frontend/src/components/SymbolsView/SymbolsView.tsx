ade769e47ac0486058a6271d255ac076d1666efe
yazonnile <yazonnile@gmail.com>
2025-02-20 23:00:10 +0200

    [update] main layout with scrollable cards view

--- frontend/src/components/SymbolsView/SymbolsView.tsx
+++ frontend/src/components/SymbolsView/SymbolsView.tsx
@@ -1,28 +1,24 @@
+import './symbolsView.css';
 import SymbolsGrid from '@/components/SymbolsGrid';
 import PriceChart from '@/components/PriceChart';
 import DesktopInfo from './src/DesktopInfo';
-import { useState } from 'react';
+import { memo } from 'react';
 
 const SymbolsView = () => {
-  const [activeSymbol, setActiveSymbol] = useState<null | string>(null);
-  const handleSymbolClick = (symbolId: string) => {
-    setActiveSymbol((s) => (s === symbolId ? null : symbolId));
-  };
-
   return (
+    <>
+      <DesktopInfo />
       <div className="symbolsView">
-        <DesktopInfo/>
-        <div className="symbolsView__chart">
+        <aside>
           <h3>PRICE HISTORY</h3>
-        </div>
+          <PriceChart />
+        </aside>
         <div className="symbolsView__content">
-          <PriceChart symbolId={activeSymbol}/>
-          <div className="symbolsView__cards">
-            <SymbolsGrid onSymbolClick={handleSymbolClick}/>
-          </div>
+          <SymbolsGrid />
         </div>
       </div>
+    </>
   );
 };
 
-export default SymbolsView;
+export default memo(SymbolsView);
