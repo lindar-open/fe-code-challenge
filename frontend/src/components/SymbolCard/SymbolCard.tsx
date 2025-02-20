da1038948ea9cea70cfb5e0a4cb2291b4d16ca4a
yazonnile <yazonnile@gmail.com>
2025-02-20 23:17:00 +0200

    [update] use hooks/store/dumb components to update SymbolCard

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -1,28 +1,72 @@
+import TrendDownSrc from '@/assets/down.png';
+import TrendUpSrc from '@/assets/up.png';
+import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import {
+  selectActiveSymbol,
+  selectShowCardInfo,
+  setActiveSymbol
+} from '@/store/dashboardOptionsSlice';
+import { lazy, memo, Suspense, useMemo } from 'react';
 import './symbolCard.css';
-import { ReactComponent as CompanyIcon } from '@/assets/company.svg';
-import { useAppSelector } from '@/hooks/redux';
-import ListItem from '@/components/ListItem';
+
+import { usePriceChangeClass } from './hooks/use-price-change-class';
+import { usePriceChangePercentage } from './hooks/use-price-change-percentage';
+
+import SymbolCardHeader from './src/SymbolCardHeader';
+import SymbolCardPriceRow from './src/SymbolCardPriceRow';
+import SymbolCardTrend from './src/SymbolCardTrend';
+import Loading from '../Loading';
+
+const SymbolCardInfoLazy = lazy(() => import('./src/SymbolCardInfo'));
 
 type SymbolCardProps = {
   id: string;
-  onClick: (symbolId: string) => void;
   price: number;
 };
 
-const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
-  const { trend, companyName } = useAppSelector((state) => state.stocks.entities[id]);
+const SymbolCard = ({ id, price }: SymbolCardProps) => {
+  const dispatch = useAppDispatch();
+  const showCardInfo = useAppSelector(selectShowCardInfo);
+  const activeSymbol = useAppSelector(selectActiveSymbol);
+  const { trend, companyName, marketCap, industry } = useAppSelector(
+    (state) => state.stocks.entities[id]
+  );
+
+  const priceChangePercentage = usePriceChangePercentage(price);
+  const rootClassName = usePriceChangeClass(
+    priceChangePercentage,
+    activeSymbol === null ? activeSymbol : activeSymbol === id
+  );
+
   const handleOnClick = () => {
-    onClick(id);
+    dispatch(setActiveSymbol(id));
   };
+
+  const trendSrc = useMemo(() => {
+    if (trend === 'UP') {
+      return TrendUpSrc;
+    } else if (trend === 'DOWN') {
+      return TrendDownSrc;
+    }
+    return null;
+  }, [trend]);
+
   return (
-    <div onClick={handleOnClick} className="symbolCard">
-      <div>
-        {id} - {trend}
+    <div onClick={handleOnClick} className={rootClassName}>
+      <SymbolCardTrend src={trendSrc} />
+
+      <SymbolCardHeader id={id} />
+
+      <div className="symbolCard__content">
+        <SymbolCardPriceRow price={price} />
+
+        {showCardInfo && (
+          <Suspense fallback={<Loading />}>
+            <SymbolCardInfoLazy companyName={companyName} marketCap={marketCap} industry={industry} />
+          </Suspense>
+        )}
       </div>
-      <div>Price:</div>
-      <div>{price || '--'} </div>
-      <ListItem Icon={<CompanyIcon />} label={companyName} />
     </div>
   );
 };
-export default SymbolCard;
+export default memo(SymbolCard);
--- frontend/src/components/SymbolsGrid/SymbolsGrid.tsx
+++ frontend/src/components/SymbolsGrid/SymbolsGrid.tsx
@@ -1,23 +1,22 @@
+import './symbolsGrid.css';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '@/hooks/redux';
 import SymbolCard from '../SymbolCard';
 import { fetchAllStocks, selectors } from '@/store/stocksSlice';
-type SymbolsGridProps = {
-  onSymbolClick: (symbolId: string) => void;
-};
 
-const SymbolsGrid = ({ onSymbolClick }: SymbolsGridProps) => {
+const SymbolsGrid = () => {
   const stockSymbols = useAppSelector(selectors.selectStockIds);
   const prices = useAppSelector((state) => state.prices);
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     dispatch(fetchAllStocks());
   }, [dispatch]);
 
   return (
-    <div>
+    <div className="symbolsGrid">
       {stockSymbols.map((id, i) => (
-        <SymbolCard price={prices[id]} onClick={onSymbolClick} key={i} id={id} />
+        <SymbolCard price={prices[id]} key={i} id={id} />
       ))}
     </div>
   );
