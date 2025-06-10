83e8460eb53b33fe8f88020750cb74c1ad33386d
Mark Dagostino <mark.dagostino98@gmail.com>
2025-06-10 23:09:20 +0200

    Refactor SymbolCard and Router components to improve code organization and lazy load views Added Extra Info on Initial Card Fixed +Info Option on Header

--- frontend/src/components/SymbolCard/SymbolCard.tsx
+++ frontend/src/components/SymbolCard/SymbolCard.tsx
@@ -1,7 +1,10 @@
 import './symbolCard.css';
 import { ReactComponent as CompanyIcon } from '@/assets/company.svg';
+import { ReactComponent as IndustryIcon } from '@/assets/industry.svg';
+import { ReactComponent as MarketCapIcon } from '@/assets/market_cap.svg';
 import { useAppSelector } from '@/hooks/redux';
 import ListItem from '@/components/ListItem';
+import { selectShowCardInfo } from '@/store/dashboardOptionsSlice';
 
 type SymbolCardProps = {
   id: string;
@@ -10,7 +13,8 @@ type SymbolCardProps = {
 };
 
 const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
-  const { trend, companyName } = useAppSelector((state) => state.stocks.entities[id]);
+  const { trend, companyName, industry, marketCap } = useAppSelector((state) => state.stocks.entities[id]);
+  const showCardInfo = useAppSelector(selectShowCardInfo);
   const handleOnClick = () => {
     onClick(id);
   };
@@ -21,7 +25,13 @@ const SymbolCard = ({ id, onClick, price }: SymbolCardProps) => {
       </div>
       <div>Price:</div>
       <div>{price || '--'} </div>
-      <ListItem Icon={<CompanyIcon />} label={companyName} />
+      {showCardInfo && (
+        <>
+          <ListItem Icon={<CompanyIcon />} label={companyName} />
+          <ListItem Icon={<IndustryIcon />} label={industry} />
+          <ListItem Icon={<MarketCapIcon />} label={marketCap.toString()} />
+        </>
+      )}
     </div>
   );
 };
